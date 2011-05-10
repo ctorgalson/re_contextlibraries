e60d61dab2b10d7f69781d2f739132ba88a21bb9
Christopher Torgalson <chris@raisedeyebrow.com>
2011-05-10 16:59:23 -0700

    First commit; works, but needs work

---
+++ js/re_slideshowbase.js
@@ -0,0 +1,5 @@
+/**
+ * @file
+ * This file provides whatever javascript logic and functions are necessary for
+ * the re_slideshowbase module.
+ */
---
+++ re_jquerycycle/js/re_jquerycycle.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * This file provides whatever javascript logic and functions are necessary for
+ * the jquery cycle plugin in the re_slideshowbase module.
+ *
+ * @see http://malsup.com/jquery/cycle/options.html
+ */
+Drupal.behaviors.re_slideshowbase_jqueryCycle = function(context) {
+  $('#block-views-re_slideshowbase-block_1 .view-display-id-block_1 > .view-content > ul')
+    .after('<div id="re_slideshowbase-view-display-id-block_1-navigation"/>')  
+    .cycle({
+  		fx: 'scrollHorz',
+  		pager: '#re_slideshowbase-view-display-id-block_1-navigation',
+  		pause: true,
+  		pauseOnPagerHover: true
+  	});
+} /* Drupal.behaviors.re_slideshowbase_jqueryCycle */
