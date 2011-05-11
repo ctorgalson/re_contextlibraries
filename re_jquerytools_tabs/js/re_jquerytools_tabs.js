b75d27b90d8e3d7cf53f985235d983eab78b1110
Christopher Torgalson <chris@raisedeyebrow.com>
2011-05-11 16:48:13 -0700

    Add hook perm, reorganize dsm; tweak default js settings

--- re_jquerytools_tabs/js/re_jquerytools_tabs.js
+++ re_jquerytools_tabs/js/re_jquerytools_tabs.js
@@ -9,19 +9,20 @@ Drupal.behaviors.re_jquerytools_tabs = function(context) {
   // Build a menu:
   $viewItems = $('#block-views-re_slideshowbase-block_1 .view-display-id-block_1 > .view-content > ul > li');
   $navList = $('<div id="re_slideshowbase-view-display-id-block_1-navigation"></div>');
-  $viewItems.each(function(i, e) {
+  $viewItems.hide().each(function(i, e) { // Start looping through items to build the nav, and make sure all items are hidden!
     $navList.append($('<a href="#">'+ (i + 1) +'</a>'));
   });
   $viewItems.parents('.view-content').append($navList);
-
-  $('#re_slideshowbase-view-display-id-block_1-navigation').tabs('#block-views-re_slideshowbase-block_1 .view-display-id-block_1 > .view-content > ul > li', {
-    // enable "cross-fading" effect
-    effect: 'fade',
-    fadeOutSpeed: 'slow',
-    // start from the beginning after the last tab
-    rotate: true
-    // use the slideshow plugin. It accepts its own configuration
-  }).slideshow({
-    autoplay: true
-  });
+  // Now, instantiate the tabs/slideshow:
+  $('#re_slideshowbase-view-display-id-block_1-navigation')
+    .tabs($viewItems, {
+      effect: 'fade', // Enable "cross-fading" effect...
+      fadeInSpeed: 1000,
+      fadeOutSpeed: 1000,
+      rotate: true // Start from the beginning after the last tab...
+    })
+    .slideshow({ // Use the slideshow plugin. It accepts its own configuration...
+      autoplay: true,
+      interval: 4000
+    });
 } /* Drupal.behaviors.re_jquerytools_tabs */
