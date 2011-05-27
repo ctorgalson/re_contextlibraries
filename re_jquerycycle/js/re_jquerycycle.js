e2b9876bda96d55d110bd38585146e1c9e623d48
Christopher Torgalson <chris@raisedeyebrow.com>
2011-05-27 15:41:20 -0700

    moved files around, in preparation for finally splitting off the last dedicated slideshow content from the module proper

--- re_jquerycycle/js/re_jquerycycle.js
+++ re_jquerycycle/js/re_jquerycycle.js
@@ -3,15 +3,18 @@
  * This file provides whatever javascript logic and functions are necessary for
  * the jquery cycle plugin in the re_contextlibraries module.
  *
+ * Originally developed for use with the re_slideshow feature. You can override
+ * this file in the context where the library is invoked.
+ *
  * @see http://malsup.com/jquery/cycle/options.html
  */
 Drupal.behaviors.re_jquerycycle = function(context) {
-  $('#block-views-re_contextlibraries-block_1 .view-display-id-block_1 > .view-content > ul')
-    .after('<div id="re_contextlibraries-view-display-id-block_1-navigation"/>')  
+  $('#block-views-re_slideshow-block_1 .view-display-id-block_1 > .view-content > ul')
+    .after('<div id="re_slideshow-view-display-id-block_1-navigation"/>')  
     .cycle({
   		fx: 'scrollVert',
   		height: 400,
-  		pager: '#re_contextlibraries-view-display-id-block_1-navigation',
+  		pager: '#re_slideshow-view-display-id-block_1-navigation',
   		pause: true,
   		pauseOnPagerHover: true
   	});
--- re_jquerytools_tabs/js/re_jquerytools_tabs.js
+++ re_jquerytools_tabs/js/re_jquerytools_tabs.js
@@ -1,17 +1,47 @@
 /**
  * @file
  * This file provides whatever javascript logic and functions are necessary for
- * the jquery cycle plugin in the re_contextlibraries module.
+ * the jquery cycle plugin in the re_slideshow module.
+ *
+ * Originally developed for use with the re_slideshow feature. You can override
+ * this file in the context where the library is invoked.
  *
  * @see http://flowplayer.org/tools/tabs/index.html#api
  */
+
+
+/**
+ * This function builds, and returns, a generic menu based on the items in a
+ * list.
+ */
+$.re_jquerytools_tabs_menu = function(options) {  
+  var defaults = {
+        slideSelector: '#block-views-re_contextlibraries-block_1 .view-display-id-block_1 > .view-content > ul > li',
+        navItemParent: '<div id="re_contextlibraries-view-display-id-block_1-navigation" class="foo" />',
+        navItem: '<a href="#" />'
+      },
+      options = $.extend(defaults, options),
+      $navElement = $(options.navItemParent),
+      $(options.slideSelector).each(function(i, e){
+        $navElement.append($(options.navItem).text(i + 1));
+      });
+  return $navElement;
+};
+/* $.re_jquerytools_tabs_menu */
+ 
+
+/**
+ * This is the main, 'business', function for this library.
+ *
+ * @see $.re_jquerytools_tabs_menu
+ */
 Drupal.behaviors.re_jquerytools_tabs = function(context) {
   // Get the selector:
-  var $viewItems = $('#block-views-re_contextlibraries-block_1 .view-display-id-block_1 > .view-content > ul > li');
+  var $viewItems = $('#block-views-re_slideshow-block_1 .view-display-id-block_1 > .view-content > ul > li');
   // Build a menu and append it at an appropriate location:
-  $viewItems.parents('.view-content').append($.re_contextlibraries_menu());
+  $viewItems.parents('.view-content').append($.re_jquerytools_tabs_menu());
   // Now, instantiate the tabs/slideshow:
-  $('#re_contextlibraries-view-display-id-block_1-navigation')
+  $('#re_slideshow-view-display-id-block_1-navigation')
     .tabs($viewItems, {
       effect: 'fade', // Enable "cross-fading" effect...
       fadeInSpeed: 1000,
