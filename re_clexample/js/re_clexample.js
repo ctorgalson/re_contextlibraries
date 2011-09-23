3a96cda028ec93d341abfd04623a574c7e67013e
bedlam <bedlamhotel@gmail.com>
2011-09-23 14:32:04 -0700

    Modify cl example module to use google fonts CSS when advagg module present

--- re_clexample/js/re_clexample.js
+++ re_clexample/js/re_clexample.js
@@ -7,10 +7,10 @@ Drupal.behaviors.re_clexample = function(context) {
   // Create our new element, complete with styles:
   $box = $('<div/>')
       .attr('id', 're_clexample')
+      .addClass(Drupal.settings.re_clexample.extendedFonts == true ? 'extended-fonts' : 'standard-fonts')
       .html('<h2>RE CL Example</h2><p>' + Drupal.t('This box demonstrates the RE CL Example javascript library provided by the RE CL Example module.' + '</p><p>' + Drupal.t('Click anywhere in the box to dismiss this message.')) + '</p>')
       .click(function(){
         $(this).hide();
       })
       .appendTo('body');
-      
 } /* Drupal.behaviors.re_clexample */
